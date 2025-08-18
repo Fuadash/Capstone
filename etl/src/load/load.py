59276da716e4a26042e0a0a3580289153fe2525b
Fua <fuadash@protonmail.com>
2025-08-18 12:53:57 +0100

    feat: updated load for csv

--- etl/src/load/load.py
+++ etl/src/load/load.py
@@ -3,14 +3,19 @@ from sqlalchemy import Engine
 
 
 # This creates the table and appends the data
-def load(df: pd.DataFrame, engine: Engine, table_name: str, schema):
+def load(
+    df: pd.DataFrame, engine: Engine, table_name: str, schema: str, size: int = 15000
+):
     """
     Imports data from a DataFrame into a database
     Is passed a DataFrame and an Engine
     Along with the target schema and table name
     """
+    # Reduce the df size to not cause issues
+    df_small = df.head(size)
+
     # Now append the data
-    df.to_sql(
+    df_small.to_sql(
         name=table_name,
         con=engine,
         schema=schema,
