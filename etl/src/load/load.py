ab15d343e7e126c17b6e3dc84d170d14997851a6
Fua <fuadash@protonmail.com>
2025-08-18 14:07:02 +0100

    feat: now can load csv data into database

--- etl/src/load/load.py
+++ etl/src/load/load.py
@@ -4,7 +4,7 @@ from sqlalchemy import Engine
 
 # This creates the table and appends the data
 def load(
-    df: pd.DataFrame, engine: Engine, table_name: str, schema: str, size: int = 15000
+    df: pd.DataFrame, engine: Engine, table_name: str, schema: str, size: int = 10000
 ):
     """
     Imports data from a DataFrame into a database
@@ -12,7 +12,12 @@ def load(
     Along with the target schema and table name
     """
     # Reduce the df size to not cause issues
-    df_small = df.head(size)
+    df_small = df.head(size).copy()
+
+    print(len(df))
+    print(len(df_small))
+
+    df_small.reset_index(inplace=True)
 
     # Now append the data
     df_small.to_sql(
@@ -22,4 +27,5 @@ def load(
         if_exists="replace",
         index=False,
         method="multi",
+        chunksize=1000,
     )
