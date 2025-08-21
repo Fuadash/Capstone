94a3600dfcfcf53865e07a158b04141f469a3356
Fua <fuadash@protonmail.com>
2025-08-21 11:40:04 +0100

    feat: now formats wth dollars on the table

--- streamlit/src/ui/tables.py
+++ streamlit/src/ui/tables.py
@@ -3,9 +3,16 @@ import pandas as pd
 
 
 def games_table(df: pd.DataFrame):
+    df = df.copy().rename(columns={'Price': 'Price ($)'})
+
+    # Format Price: 0 -> "FREE"
+    df["Price ($)"] = df["Price ($)"].apply(
+        lambda x: "FREE" if x == 0 else f"${x:.2f}"
+    )
+
     st.subheader("Steam Store Games")
     st.dataframe(
-        df[["Name", "Price", "Tags", "Sentiment", "Release Year", "Developers", "Available platforms"]].head(500),
+        df[["Name", "Price ($)", "Tags", "Sentiment", "Peak CCU", "Release Year", "Developers", "Available platforms"]].head(500),
         use_container_width=True,
         hide_index=True,
     )
